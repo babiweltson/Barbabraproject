ec24d0a0b7eda41b34ae456d6138887c9a4924d6
babiweltson <weltsonbarbara@gmail.com>
2020-09-29 19:02:47 +0100

    Adding for and trying to get the signatureId (with no sucess)

--- src/main/java/com/barbara/GetEmbeddedURL.java
+++ src/main/java/com/barbara/GetEmbeddedURL.java
@@ -9,8 +9,8 @@ public class GetEmbeddedURL {
 	public static void main(String[] args) throws HelloSignException {
 	HelloSignClient client = new HelloSignClient("c15ea3a35f926cc6a0c495e2bc019543e49f9d2f41019adc0f3fbb6909bddfa6");
 	String signatureId = SendEmbeddedSignatureRequest.signatureId;
-	//EmbeddedResponse response = client.getEmbeddedSignUrl(signatureId);
-	//String url = response.getSignUrl();
+	EmbeddedResponse response = client.getEmbeddedSignUrl(signatureId);
+	String url = response.getSignUrl();
 	System.out.println(signatureId);
 	
 	}
