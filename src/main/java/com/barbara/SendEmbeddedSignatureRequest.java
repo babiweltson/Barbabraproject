5dbe6b5c220a0c534bbd9b0d84d15f06bcb31b7b
babiweltson <weltsonbarbara@gmail.com>
2020-09-29 18:34:42 +0100

    hi

--- src/main/java/com/barbara/SendEmbeddedSignatureRequest.java
+++ src/main/java/com/barbara/SendEmbeddedSignatureRequest.java
@@ -7,14 +7,14 @@ import org.json.JSONObject;
 import com.hellosign.sdk.HelloSignClient;
 import com.hellosign.sdk.HelloSignException;
 import com.hellosign.sdk.resource.EmbeddedRequest;
-import com.hellosign.sdk.resource.EmbeddedResponse;
 import com.hellosign.sdk.resource.SignatureRequest;
 
 public class SendEmbeddedSignatureRequest {
-
+	
+	public static String signatureId;
+	
 	public static void main(String[] args) throws HelloSignException {
 		SignatureRequest request = new SignatureRequest();
-		
 		request.setTitle("Test Babs");
 		request.setSubject("The NDA we talked about");
 		request.setMessage("Let me know if you have any questions.");
@@ -24,19 +24,33 @@ public class SendEmbeddedSignatureRequest {
 		request.addFile(new File("/Users/bweltson/Documents/Template Signature.docx"));
 		request.setTestMode(true);
 
+	
+		
+
 		String clientId = "e32a44e8f4f09361a9a73586e3811c1a";
 		EmbeddedRequest embedReq = new EmbeddedRequest(clientId, request);
 
 		HelloSignClient client = new HelloSignClient("c15ea3a35f926cc6a0c495e2bc019543e49f9d2f41019adc0f3fbb6909bddfa6");
 		SignatureRequest newRequest = (SignatureRequest) client.createEmbeddedRequest(embedReq);
 		
-		System.out.println(newRequest.getSignature("bweltson@hellosign.com", "Barbara 1").getId());
 		
-		System.out.println(newRequest.getJSONObject());
-		
+
+			for(Object signature : newRequest.getJSONObject().getJSONArray("signatures") ) {    
+					
+				 signatureId = ((JSONObject) signature).getString("signature_id");
+	
+			}
+			
+			System.out.println(signatureId);
 	
 	}
-	
 
+	public static  String getSignatureId() {
+	
+		return signatureId;
+		
+	}
+
+	
 }
 
--- src/main/java/com/barbara/SendEmbeddedWithTemplate.java
+++ src/main/java/com/barbara/SendEmbeddedWithTemplate.java
@@ -1,5 +1,7 @@
 package com.barbara;
 
+import org.json.JSONObject;
+
 import com.hellosign.sdk.HelloSignClient;
 import com.hellosign.sdk.HelloSignException;
 import com.hellosign.sdk.resource.EmbeddedRequest;
@@ -7,7 +9,7 @@ import com.hellosign.sdk.resource.SignatureRequest;
 import com.hellosign.sdk.resource.TemplateSignatureRequest;
 
 public class SendEmbeddedWithTemplate {
-
+	
 	public static void main(String[] args) throws HelloSignException {
 		
 		TemplateSignatureRequest request = new TemplateSignatureRequest();
@@ -15,17 +17,30 @@ public class SendEmbeddedWithTemplate {
 		request.setSubject("Hello Test");
 		request.setMessage("Please sign this ASAP");
 		request.setSigner("Client", "bweltson@hellosign.com", "Babs");
-		request.setCustomFieldValue("Cost", "$20,000");
 		request.setTestMode(true);
+		
+		
 
 		String clientId = "e32a44e8f4f09361a9a73586e3811c1a";
 		EmbeddedRequest embedReq = new EmbeddedRequest(clientId, request);
 
 		HelloSignClient client = new HelloSignClient("c15ea3a35f926cc6a0c495e2bc019543e49f9d2f41019adc0f3fbb6909bddfa6");
 		SignatureRequest newRequest = (SignatureRequest) client.createEmbeddedRequest(embedReq);
-		System.out.println(newRequest.getJSONObject());
+		
 
-
+		for(Object signature : newRequest.getJSONObject().getJSONArray("signatures") ) {    
+			SendEmbeddedSignatureRequest.signatureId = ((JSONObject) signature).getString("signature_id");
+			}
+		
+			
+			System.out.println(SendEmbeddedSignatureRequest.signatureId);
+			
 	}
 
+
+	
+		
+
 }
+
+
